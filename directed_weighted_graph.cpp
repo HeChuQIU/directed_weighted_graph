f00200afad1ae26b822fef590fbdf878bfea73ee
HeChu <2296282722@qq.com>
2023-12-18 17:53:44 +0800

    完成应用

--- directed_weighted_graph.cpp
+++ directed_weighted_graph.cpp
@@ -6,61 +6,157 @@
 #include "ranges"
 #include <algorithm>
 
-void print_graph(const directed_weighted_graph<int> &graph) {
+void print_graph(const auto &graph) {
     std::cout << "图信息：" << std::endl;
     std::cout << std::endl;
 
-    std::cout << "节点信息: " << std::endl
-              << "节点数量：" << graph.nodes.size() << std::endl;
-    std::ranges::for_each(graph.nodes, [](directed_weighted_graph<int>::node *n) {
-        std::cout << "节点 " << n->value << std::endl;
+    std::cout << "  节点信息: " << std::endl
+              << "    节点数量：" << graph.nodes.size() << std::endl;
+    std::ranges::for_each(graph.nodes, [](auto *n) {
+        std::cout << "  节点 " << n->value << std::endl;
     });
     std::cout << std::endl;
 
-    std::cout << "边信息: " << std::endl
-              << "边数量：" << graph.edges.size() << std::endl;
-    std::ranges::for_each(graph.edges, [](directed_weighted_graph<int>::edge *e) {
-        std::cout << "边 " << e->from->value << "->" << e->to->value << ": " << e->weight
+    std::cout << "  边信息: " << std::endl
+              << "    边数量：" << graph.edges.size() << std::endl;
+    std::ranges::for_each(graph.edges, [](auto *e) {
+        std::cout << "      边 " << e->from->value << "->" << e->to->value << ": " << e->weight
                   << std::endl;
     });
     std::cout << std::endl;
 }
 
 int main() {
-    directed_weighted_graph<int> graph;
+    std::cout << "这是一个有向带权图的演示程序" << std::endl
+              << std::endl;
 
-    std::vector<directed_weighted_graph<int>::node *> nodes;
+    directed_weighted_graph<std::string> graph;
+    int choice = -1;
+    while (choice != 0) {
+        std::cout << "1. 添加节点" << std::endl
+                  << "2. 添加/编辑边" << std::endl
+                  << "3. 删除节点" << std::endl
+                  << "4. 删除边" << std::endl
+                  << "5. 查看图信息" << std::endl
+                  << "6. 计算最短路程" << std::endl
+                  << "0. 退出" << std::endl
+                  << "请选择操作：";
+        std::cin >> choice;
+        std::cout << std::endl;
+        switch (choice) {
+            case 1: {
+                std::cout << "请输入节点名：";
+                std::string value;
+                std::cin >> value;
+                if (graph.find_node_or_else_null(value) != nullptr) {
+                    std::cout << "已存在同名节点" << std::endl;
+                    break;
+                }
+                graph.create_node(value);
+                std::cout << "已添加节点 " << value << std::endl;
+                break;
+            }
+            case 2: {
+                std::cout << "请输入边的起点名：";
+                std::string from, to;
+                std::cin >> from;
+                auto from_node = graph.find_node_or_else_null(from);
+                if (from_node == nullptr) {
+                    std::cout << "不存在该节点" << std::endl;
+                    break;
+                }
+                std::cout << "请输入边的终点名：";
+                std::cin >> to;
+                auto to_node = graph.find_node_or_else_null(to);
+                if (to_node == nullptr) {
+                    std::cout << "不存在该节点" << std::endl;
+                    break;
+                }
+                std::cout << "请输入边的权重：";
+                int weight;
+                std::cin >> weight;
+                graph.set_weight(from_node, to_node, weight);
+                std::cout << "设置了边 " << from << "->" << to << ": " << weight << std::endl;
+                break;
+            }
+            case 3: {
+                std::cout << "请输入要删除的节点名：";
+                std::string value;
+                std::cin >> value;
+                auto node = graph.find_node_or_else_null(value);
+                if (node == nullptr) {
+                    std::cout << "不存在该节点" << std::endl;
+                    break;
+                }
+                graph.remove_node(node);
+                std::cout << "已删除节点 " << value << std::endl;
+                break;
+            }
+            case 4: {
+                std::cout << "请输入要删除的边的起点名：";
+                std::string from, to;
+                std::cin >> from;
+                auto from_node = graph.find_node_or_else_null(from);
+                if (from_node == nullptr) {
+                    std::cout << "不存在该节点" << std::endl;
+                    break;
+                }
+                std::cout << std::endl;
+                std::cout << "请输入要删除的边的终点名：";
+                std::cin >> to;
+                auto to_node = graph.find_node_or_else_null(to);
+                if (to_node == nullptr) {
+                    std::cout << "不存在该节点" << std::endl;
+                    break;
+                }
+                graph.remove_edge(from_node, to_node);
+                std::cout << "已删除边 " << from << "->" << to << std::endl;
+                break;
+            }
+            case 5: {
+                print_graph(graph);
+                break;
+            }
+            case 6: {
+                std::cout << "请输入起点名：";
+                std::string from, to;
+                std::cin >> from;
+                auto from_node = graph.find_node_or_else_null(from);
+                if (from_node == nullptr) {
+                    std::cout << "不存在该节点" << std::endl;
+                    break;
+                }
+                std::cout << "请输入终点名（不输入终点名则输出到所有终点的最短路程）：";
+                std::getline(std::cin, to);
+                std::getline(std::cin, to);
 
-    for (int i = 0; i < 10; ++i) {
-        nodes.push_back(graph.create_node(i));
-    }
+                auto to_node = graph.find_node_or_else_null(to);
+                if (!to.empty() && to_node == nullptr) {
+                    std::cout << "不存在该节点" << std::endl;
+                    break;
+                }
 
-    graph.set_weight(nodes[0], nodes[1], 3);
-    graph.set_weight(nodes[1], nodes[2], 1);
-    graph.set_weight(nodes[2], nodes[0], 9);
-    graph.set_weight(nodes[2], nodes[3], 6);
-    graph.set_weight(nodes[1], nodes[3], 1);
-    graph.set_weight(nodes[0], nodes[3], -1);
-    graph.set_weight(nodes[3], nodes[0], 1);
-
-    // 输出图信息
-    print_graph(graph);
-
-    try {
-        auto start = nodes[0];
-        auto distances = graph.shortest_paths_spfa(start);
-        std::cout << "最短距离：" << std::endl;
-        // 输出最短路径长度
-        for (const auto &entry: distances) {
-            std::cout << start->value << "->" << entry.first->value << ": "
-                      << (entry.second == std::numeric_limits<int>::max() ? "不可达" : std::to_string(
-                              entry.second))
-                      << std::endl;
+                auto distance = graph.shortest_paths_spfa(from_node);
+                std::cout << "最短路程：" << std::endl;
+                if (to.empty()) {
+                    std::ranges::for_each(distance, [](auto pair) {
+                        std::cout << "  " << pair.first->value << ": " << pair.second << std::endl;
+                    });
+                } else {
+                    std::cout << "  " << to << ": " << distance[to_node] << std::endl;
+                }
+                break;
+            }
+            case 0: {
+                std::cout << "退出程序" << std::endl;
+                break;
+            }
+            default: {
+                std::cout << "无效的操作" << std::endl;
+                break;
+            }
         }
+        std::cout << std::endl;
     }
-    catch (std::runtime_error err) {
-        std::cerr << err.what();
-    }
-
     return 0;
 }
--- directed_weighted_graph.h
+++ directed_weighted_graph.h
@@ -11,6 +11,7 @@
 #include <limits>
 #include <iostream>
 #include <queue>
+#include <unordered_set>
 
 template<class T>
 class directed_weighted_graph {
@@ -29,7 +30,7 @@ public:
         return new_node;
     }
 
-    void set_weight(node *from, node *to, int weight) {
+    void set_weight(node* from, node* to, int weight) {
         if (std::find(nodes.begin(), nodes.end(), from) == nodes.end())
             nodes.push_back(from);
         if (std::find(nodes.begin(), nodes.end(), to) == nodes.end())
@@ -43,12 +44,25 @@ public:
         auto edge = find_edge(from, to);
         if (edge != edges.end())
             edges.erase(edge);
+        else{
+            throw std::runtime_error("边不存在");
+        }
+    }
+
+    void remove_node(node *n) {
+        auto node_iter = std::find(nodes.begin(), nodes.end(), n);
+        if (node_iter != nodes.end())
+            nodes.erase(node_iter);
+        for (auto *e : edges) {
+            if (e->from == n || e->to == n) {
+                remove_edge(e->from, e->to);
+            }
+        }
     }
 
     std::unordered_map<node *, int> shortest_paths_spfa(node *start) {
         std::unordered_map<node *, int> distance;
         std::unordered_map<node *, int> in_queue_count;
-
         for (auto *n : nodes) {
             distance[n] = std::numeric_limits<int>::max();
             in_queue_count[n] = 0;
@@ -56,24 +70,24 @@ public:
         distance[start] = 0;
 
         std::queue<node *> q;
-        std::vector<bool> in_queue(nodes.size(), false);
+        std::unordered_set<node *> in_queue;
 
         q.push(start);
-        in_queue[start->value] = true;
+        in_queue.insert(start);
 
         while (!q.empty()) {
             auto *current = q.front();
             q.pop();
-            in_queue[current->value] = false;
+            in_queue.erase(current);
 
             for (auto *e : edges) {
                 if (distance[e->from] != std::numeric_limits<int>::max() &&
                     distance[e->to] > distance[e->from] + e->weight) {
                     distance[e->to] = distance[e->from] + e->weight;
 
-                    if (!in_queue[e->to->value]) {
+                    if (in_queue.find(e->to) == in_queue.end()) {
                         q.push(e->to);
-                        in_queue[e->to->value] = true;
+                        in_queue.insert(e->to);
                         in_queue_count[e->to]++;
                         if (in_queue_count[e->to] >= nodes.size()) {
                             // 存在负权环
@@ -87,7 +101,26 @@ public:
         return distance;
     }
 
+    node* find_node_or_else_null(T value) {
+        auto node = std::find_if(nodes.begin(), nodes.end(),
+                                 [value](const directed_weighted_graph::node *node) {
+                                     return node->value == value;
+                                 });
+        return node != nodes.end() ? *node : nullptr;
+    }
 
+    auto find_edge(node *from, node *to) {
+        auto edge = std::find_if(edges.begin(), edges.end(),
+                                 [from, to](const directed_weighted_graph::edge *edge) {
+                                     return edge->from == from && edge->to == to;
+                                 });
+        return edge;
+    }
+
+    auto find_edge_or_else_create(node *from, node *to) {
+        auto edge_iter = find_edge(from, to);
+        return (edge_iter != edges.end()) ? *edge_iter : new directed_weighted_graph::edge(from, to);
+    }
 
     class node {
     public:
@@ -110,20 +143,6 @@ public:
             this->weight = weight;
         }
     };
-
-private:
-    auto find_edge(node *from, node *to) {
-        auto edge = std::find_if(edges.begin(), edges.end(),
-                                 [from, to](const directed_weighted_graph::edge *edge) {
-                                     return edge->from == from && edge->to == to;
-                                 });
-        return edge;
-    }
-
-    auto find_edge_or_else_create(node *from, node *to) {
-        auto edge_iter = find_edge(from, to);
-        return (edge_iter != edges.end()) ? *edge_iter : new directed_weighted_graph::edge(from, to);
-    }
 };
 
 
